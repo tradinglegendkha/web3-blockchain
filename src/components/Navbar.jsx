199bf13839e57a07a4f3e0bb7e9214bde963156c
Buppi Khan <khanbuppi@gmail.com>
2024-06-15 19:34:54 -0400

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+// import { AiOutlineClose } from "react-icons/ai";
+// import { HimenuAlt4 } from "react-icons/hi";
+
+import logo from "../assets/logo.png";
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1>Navbar</h1>
+    </div>
+  );
+};
+
+export default Navbar;
